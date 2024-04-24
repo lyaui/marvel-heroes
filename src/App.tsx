a0bc434f3e2cf856a992ddd772705bafa9f90435
Iris Huang <lyaui@outlook.com>
2024-04-25 02:23:01 +0800

    feat(App): add QueryClientProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+import HeroList from '@/components/HeroList';
+
+const queryClient = new QueryClient();
+
 function App() {
-  return <>hello</>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      ... <HeroList />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
