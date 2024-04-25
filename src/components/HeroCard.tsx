3e7b8684604920a37050536ddb7a16165bcd2f95
Iris Huang <peiyu.huang@mile.cloud>
2024-04-25 14:47:10 +0800

    feat: create HeroCard

---
+++ src/components/HeroCard.tsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom';
+
+import type { Hero } from '@/types/hero';
+import { ROUTER_PATH } from '@/routes';
+
+type HeroCardProps = { hero: Hero };
+
+const HeroCard = ({ hero }: HeroCardProps) => {
+  const { name = '--', image = '', id = '' } = hero;
+
+  return (
+    <Link to={ROUTER_PATH.HERO_PROFILE.replace(':heroId', id)}>
+      <p>{name}</p>
+      <img src={image} alt={name} />
+    </Link>
+  );
+};
+
+export default HeroCard;
