6853e5c9c5dca1c25861177bb2fdd976f0498620
Iris Huang <peiyu.huang@mile.cloud>
2024-04-25 15:40:46 +0800

    feat(HeroProfilePage): get hero profile and render values

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from 'react-router-dom';
 import { router } from '@/routes';
-const queryClient = new QueryClient();
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 600000, // 10 mins
+      gcTime: 900000, // 15 mins
+    },
+  },
+});
 
 function App() {
   return (
--- src/pages/HeroProfilePage.tsx
+++ src/pages/HeroProfilePage.tsx
@@ -1,5 +1,27 @@
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+
+import type { HeroProfile } from '@/types/hero';
+import { apiGetHeroProfile } from '@/api/heroes';
+
 function HeroProfilePage() {
-  return <div>HeroProfilePage</div>;
+  const { heroId = '' } = useParams();
+
+  const { isPending, data = {} as HeroProfile } = useQuery({
+    enabled: !!heroId,
+    queryKey: [heroId],
+    queryFn: () => apiGetHeroProfile(heroId),
+  });
+
+  if (isPending) return 'Loading';
+
+  return (
+    <div>
+      {Object.entries(data).map(([_key, _value]) => (
+        <div>{`${_key ? _key.toUpperCase() : '--'}: ${_value ?? '--'}`}</div>
+      ))}
+    </div>
+  );
 }
 
 export default HeroProfilePage;
