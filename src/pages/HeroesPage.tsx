12fdbf6856b0d364ae643ea35bf98e7c1bc05255
Iris Huang <peiyu.huang@mile.cloud>
2024-04-25 14:48:17 +0800

    feat(HeroesPage): get heros and render list

--- src/pages/HeroesPage.tsx
+++ src/pages/HeroesPage.tsx
@@ -1,11 +1,22 @@
 import { Outlet } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+
+import { apiGetHeroList } from '@/api/heroes';
+import HeroList from '@/components/HeroList';
 
 function HeroesPage() {
+  const { isFetching, data = [] } = useQuery({
+    queryKey: [],
+    queryFn: apiGetHeroList,
+  });
+
+  if (isFetching) return 'Loading...';
+
   return (
-    <div>
-      HeroesPage
+    <>
+      <HeroList list={data} />
       <Outlet />
-    </div>
+    </>
   );
 }
 
