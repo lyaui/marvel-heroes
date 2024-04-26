6b58a2d51ade4a87f8e015033cf6a12e62e76ee9
Iris Huang <peiyu.huang@mile.cloud>
2024-04-26 10:37:44 +0800

    fix: move get heroes logic down level

--- src/components/HeroList.tsx
+++ src/components/HeroList.tsx
@@ -1,11 +1,35 @@
-import type { ReactNode } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
 
-type HeroListProps = {
-  children: ReactNode;
-};
+import { apiGetHeroList } from '@/api/heroes';
+import HeroCard from '@/components/HeroCard';
 
-const HeroList = ({ children }: HeroListProps) => {
-  return <ul style={{ display: 'flex', flexWrap: 'wrap' }}>{children}</ul>;
+const HeroList = () => {
+  const navigate = useNavigate();
+  const { heroId } = useParams();
+
+  const { isLoading, data = [] } = useQuery({
+    queryKey: [],
+    queryFn: apiGetHeroList,
+  });
+
+  const onClick = (id: string) => {
+    if (heroId === id) return;
+    navigate(`/heroes/${id}`);
+  };
+
+  if (isLoading) return 'Loading...';
+  if (data.length === 0) return 'No Results';
+
+  return (
+    <ul style={{ display: 'flex', flexWrap: 'wrap' }}>
+      {data.map((_hero) => (
+        <li key={_hero.id} onClick={() => onClick(_hero.id)}>
+          <HeroCard hero={_hero} isSelected={heroId === _hero.id} />
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default HeroList;
--- src/pages/HeroesPage.tsx
+++ src/pages/HeroesPage.tsx
@@ -1,37 +1,11 @@
-import { useNavigate, useParams, Outlet } from 'react-router-dom';
-import { useQuery } from '@tanstack/react-query';
+import { Outlet } from 'react-router-dom';
 
-import { apiGetHeroList } from '@/api/heroes';
 import HeroList from '@/components/HeroList';
-import HeroCard from '@/components/HeroCard';
 
 function HeroesPage() {
-  const navigate = useNavigate();
-  const { heroId } = useParams();
-
-  const { isLoading, data = [] } = useQuery({
-    queryKey: [],
-    queryFn: apiGetHeroList,
-  });
-
-  const onClick = (id: string) => {
-    if (heroId === id) return;
-    navigate(`/heroes/${id}`);
-  };
-
-  if (isLoading) return 'Loading...';
-  if (data.length === 0) return 'No Results';
-
   return (
     <>
-      <HeroList>
-        {data.map((_hero) => (
-          <li key={_hero.id} onClick={() => onClick(_hero.id)}>
-            <HeroCard hero={_hero} isSelected={heroId === _hero.id} />
-          </li>
-        ))}
-      </HeroList>
-
+      <HeroList />
       <Outlet />
     </>
   );
