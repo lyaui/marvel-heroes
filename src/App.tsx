85a88ac6b5c67ef2953dae28ced2f1bdb6213e7b
Iris Huang <lyaui@outlook.com>
2024-04-27 04:07:39 +0800

    style(App): add SContainer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from 'react-router-dom';
+import styled from 'styled-components';
+
 import { router } from '@/routes';
+
+const SContainer = styled.div`
+  max-width: 1200px;
+  height: 100vh;
+  padding: 20px;
+  margin: 0 auto;
+`;
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -14,7 +23,9 @@ const queryClient = new QueryClient({
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <SContainer>
+        <RouterProvider router={router} />
+      </SContainer>
     </QueryClientProvider>
   );
 }
