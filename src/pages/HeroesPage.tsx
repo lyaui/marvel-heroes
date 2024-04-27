3a446d060b2def44a79f421af948b3ab5a6cf702
Iris Huang <lyaui@outlook.com>
2024-04-28 02:12:28 +0800

    style(Heroes): add SSectionTitle

--- src/pages/HeroesPage.tsx
+++ src/pages/HeroesPage.tsx
@@ -1,11 +1,43 @@
 import { Outlet } from 'react-router-dom';
+import styled from 'styled-components';
 
 import HeroList from '@/components/HeroList';
+
+const SSectionTitle = styled.h3`
+  height: 66px;
+  line-height: 66px;
+  position: relative;
+  text-transform: uppercase;
+  font-size: 24px;
+  font-weight: 700;
+  letter-spacing: 1px;
+
+  &:before,
+  &:after {
+    content: '';
+    position: absolute;
+    background-color: #f5c772;
+    height: 3px;
+    transform: rotate(-45deg) skewX(45deg);
+  }
+
+  &:before {
+    top: 10px;
+    left: 40px;
+    width: 20px;
+  }
+
+  &:after {
+    bottom: 10px;
+    left: 0px;
+    width: 20px;
+  }
+`;
 
 function HeroesPage() {
   return (
     <>
-      HEROS
+      <SSectionTitle>marvel heroes</SSectionTitle>
       <HeroList />
       <Outlet />
     </>
