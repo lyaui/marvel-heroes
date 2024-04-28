66fb214ad5a1022a42c290e17dc6ce95dba364c2
Iris Huang <lyaui@outlook.com>
2024-04-28 21:00:33 +0800

    style(App): add SHeader

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,40 @@ import { RouterProvider } from 'react-router-dom';
 import styled from 'styled-components';
 
 import { router } from '@/routes';
+import img_dsk from '/public/assets/images/header_bg_dsk.jpg';
 
-const SContainer = styled.div`
+const SHeader = styled.header`
+  height: 240px;
+  background-image: linear-gradient(
+      to bottom,
+      rgba(0, 0, 0, 0.7),
+      rgba(230, 36, 41, 0.2)
+    ),
+    url(${img_dsk});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  color: white;
+  text-align: center;
+  font-size: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  & h2 {
+    text-transform: uppercase;
+    font-size: 40px;
+    font-weight: 800;
+    letter-spacing: 5px;
+  }
+`;
+
+export const SContainer = styled.div`
   max-width: 1200px;
-  height: 100vh;
   padding: 20px;
   margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
 `;
 
 const queryClient = new QueryClient({
@@ -23,9 +51,16 @@ const queryClient = new QueryClient({
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <SContainer>
-        <RouterProvider router={router} />
-      </SContainer>
+      <SHeader>
+        <SContainer>
+          <h2>marvel</h2>
+          <p>
+            Get hooked on a hearty helping of heroes and villains from the
+            humble House of Ideas!
+          </p>
+        </SContainer>
+      </SHeader>
+      <RouterProvider router={router} />
     </QueryClientProvider>
   );
 }
