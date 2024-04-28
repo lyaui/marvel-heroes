83d3d8e401f1468b901524f75952ed564fa219ea
Iris Huang <lyaui@outlook.com>
2024-04-28 21:00:02 +0800

    style(AbilityCounter): add SAbilityCounter

--- src/components/AbilityCounter.tsx
+++ src/components/AbilityCounter.tsx
@@ -1,5 +1,35 @@
 import { memo } from 'react';
+import styled from 'styled-components';
+
 import type { HeroProfile } from '@/types/hero';
+
+export const SAbilityCounter = styled.div`
+  width: 150px;
+  display: flex;
+  flex-direction: column;
+  color: #ffffff;
+  text-align: center;
+  gap: 10px;
+  opacity: 0.7;
+  transition: all 0.3s;
+  &:hover {
+    opacity: 1;
+  }
+`;
+
+const SFigure = styled.span`
+  color: #ffffff;
+  font-size: 80px;
+  font-weight: 700;
+`;
+
+const SCounterBtn = styled.button`
+  background-color: unset;
+  border: none;
+  color: white;
+  font-size: 20px;
+  cursor: pointer;
+`;
 
 type AbilityCounterProps = {
   name: keyof HeroProfile;
@@ -21,12 +51,16 @@ const AbilityCounter = memo(
     };
 
     return (
-      <div>
-        <span>{`${name ? name.toUpperCase() : '--'}:`}</span>
-        <button onClick={handleDecrementClick}>-</button>
-        <span>{value}</span>
-        <button onClick={handleIncrementClick}>+</button>
-      </div>
+      <SAbilityCounter>
+        <SCounterBtn onClick={handleIncrementClick}>
+          <i className='fa-solid fa-caret-up'></i>
+        </SCounterBtn>
+        <SFigure>{value}</SFigure>
+        <SCounterBtn onClick={handleDecrementClick}>
+          <i className='fa-solid fa-caret-down'></i>
+        </SCounterBtn>
+        <span>{`${name ? name.toUpperCase() : '--'}`}</span>
+      </SAbilityCounter>
     );
   },
 );
