b96d264ba5b48225c41bf1e9a1b5a730aa807bdc
Iris Huang <lyaui@outlook.com>
2024-04-28 21:16:52 +0800

    style(HeroProfilePage): add SHeroProfileSection

--- src/pages/HeroProfilePage.tsx
+++ src/pages/HeroProfilePage.tsx
@@ -1,14 +1,37 @@
 import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
 
+import { SContainer } from '@/App.tsx';
+import { SSectionTitle } from '@/pages/HeroesPage';
 import AbilityPanel from '@/components/AbilityPanel';
+
+const SHeroProfileSection = styled.section`
+  background-color: #151515;
+  padding: 20px 0;
+  margin-top: 50px;
+  background-image: linear-gradient(
+      to bottom,
+      rgba(0, 0, 0, 0.7),
+      rgba(0, 0, 0, 0.7)
+    ),
+    url(http://i.annihil.us/u/prod/marvel/i/mg/6/90/537ba6d49472b/standard_xlarge.jpg);
+
+  background-repeat: no-repeat;
+  background-position: right;
+  background-size: contain;
+  color: white;
+`;
 
 function HeroProfilePage() {
   const { heroId = '' } = useParams();
 
   return (
-    <>
-      <AbilityPanel heroId={heroId} />
-    </>
+    <SHeroProfileSection>
+      <SContainer>
+        <SSectionTitle color='light'>Profile</SSectionTitle>
+        <AbilityPanel heroId={heroId} />
+      </SContainer>
+    </SHeroProfileSection>
   );
 }
 
