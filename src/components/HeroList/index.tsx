8095faccfb5b766049ab896f1786fdeec5f7784b
Iris Huang <lyaui@outlook.com>
2024-04-30 02:14:43 +0800

    feat(HeroList): add skeleton

--- src/components/HeroList/index.tsx
+++ src/components/HeroList/index.tsx
@@ -1,11 +1,23 @@
 import { useNavigate, useParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
 
 import { SHeroList } from '@/components/HeroList/styled';
 import QUERY_KEYS from '@/constants/queryKeys';
 import { apiGetHeroList } from '@/api/heroes';
 import Typography from '@/components/UI/Typography';
 import HeroCard from '@/components/HeroCard';
+
+export const SkeletonHeroList = () => (
+  <SkeletonTheme baseColor='#cecece' highlightColor='#bebebe'>
+    {Array.from(Array(5).keys()).map((index) => (
+      <div key={index}>
+        <Skeleton style={{ marginBottom: '10px' }} height={200} width='100%' />
+        <Skeleton height={50} width='100%' />
+      </div>
+    ))}
+  </SkeletonTheme>
+);
 
 const HeroList = () => {
   const navigate = useNavigate();
@@ -21,7 +33,14 @@ const HeroList = () => {
     navigate(`/heroes/${id}`);
   };
 
-  if (isLoading) return 'Loading...';
+  if (isLoading) {
+    return (
+      <SHeroList>
+        <SkeletonHeroList />
+      </SHeroList>
+    );
+  }
+
   if (data.length === 0) {
     return (
       <Typography style={{ textAlign: 'center', padding: 40 }}>
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import '@/reset.css';
+import 'react-loading-skeleton/dist/skeleton.css';
 import App from '@/App.tsx';
 import { GlobalStyle } from '@/GlobalStyle';
 
